303a39974668f0d31f07807b684c24af338b1aaf
Defou1t <pasnkevikn@gmail.com>
2022-10-17 11:06:44 +0300

    Initialized and configured Firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { initializeFirebase } from './services/KodikService/firebase';
 
 import './styles/index.scss'
 import store from './store/store';
@@ -8,6 +9,7 @@ import App from './components/app/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+initializeFirebase();
 
 root.render(
   <Provider store={store}>
