be89286a1a50faf3513ab95c189e66d2c9895ac7
Defou1ts <pasnkevikn@gmail.com>
2022-12-11 10:53:11 +0300

    fix active review icons

--- src/components/animePage/animeReviewItem/AnimeReviewItem.js
+++ src/components/animePage/animeReviewItem/AnimeReviewItem.js
@@ -4,14 +4,47 @@ import { useUserService } from '../../../services/auth/user.service';
 
 import likeIcon from '../../../assets/like.svg';
 import dislikeIcon from '../../../assets/dislike.svg';
+import activeDislike from '../../../assets/activeDisLike.svg';
+import activeLike from '../../../assets/activeLike.svg';
+
 import './animeReviewItem.scss';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
 
 const AnimeReviewItem = (review) => {
 	const { logDate } = useLog();
 	const { likeReview, disLikeReview } = useUserService();
 
+	const [likeSrc, setLikeSrc] = useState(likeIcon);
+	const [dislikeSrc, setDislikeSrc] = useState(dislikeIcon);
+
 	const { username, image } = review.from[0];
 	const { description, likes, dislikes, _id, createdAt } = review;
+
+	const likedComments = useSelector((state) => state.global.user?.likedComments);
+	const disLikedComments = useSelector((state) => state.global.user?.disLikedComments);
+
+	console.log(likedComments);
+
+	useEffect(() => {
+		if (likedComments) {
+			for (const commentId of likedComments) {
+				if (commentId === _id) {
+					setLikeSrc(activeLike);
+				}
+			}
+		}
+	}, [likedComments, _id]);
+
+	useEffect(() => {
+		if (disLikedComments) {
+			for (const commentId of disLikedComments) {
+				if (commentId === _id) {
+					setDislikeSrc(activeDislike);
+				}
+			}
+		}
+	}, [disLikedComments, _id]);
 
 	const timeRenderOptions = {
 		hour: 'numeric',
@@ -34,11 +67,11 @@ const AnimeReviewItem = (review) => {
 				<div className="reviews__btns">
 					<button className="reviews__answer">Ответить </button>
 					<button className="reviews__like">
-						<img src={likeIcon} alt="Likes" onClick={() => likeReview(_id)} />
+						<img src={likeSrc} alt="Likes" onClick={() => likeReview(_id)} />
 						<span>{likes}</span>
 					</button>
 					<button className="reviews__dislike">
-						<img src={dislikeIcon} alt="DisLikes" onClick={() => disLikeReview(_id)} />
+						<img src={dislikeSrc} alt="DisLikes" onClick={() => disLikeReview(_id)} />
 						<span>{dislikes}</span>
 					</button>
 				</div>
